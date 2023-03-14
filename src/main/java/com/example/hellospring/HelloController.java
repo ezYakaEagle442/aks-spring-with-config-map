f2dafa702a77d8e8fedd6ef017fd4d566cbb4c4e
ezYakaEagle442 <stepin.devops@outlook.com>
2023-03-14 16:57:54 +0100

    add base code for Hello World

---
+++ src/main/java/com/example/hellospring/HelloController.java
@@ -0,0 +1,21 @@
+package com.example.hellospring;
+
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.RequestMapping;
+// import org.slf4j.Logger;
+
+@RestController
+public class HelloController {
+
+    @RequestMapping("/")
+    public String index() {
+        Logger logger = LoggerFactory.getLogger(HelloController.class);
+        logger.trace("+++ TRACE Message");
+        logger.debug("+++ DEBUG Message");
+        logger.info("+++ INFO Message");
+        logger.warn("+++ WARN Message");
+        logger.error("+++ ERROR Message");        
+        return "Greetings from Azure !  Check out the Logs to see the output...";
+    }
+
+}
