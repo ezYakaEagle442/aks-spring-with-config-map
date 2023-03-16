142ab0d208618ffdb5b2920f9099e4b7e403b8f5
ezYakaEagle442 <stepin.devops@outlook.com>
2023-03-16 11:58:02 +0100

    typo

--- src/main/java/com/example/hellospring/HelloController.java
+++ src/main/java/com/example/hellospring/HelloController.java
@@ -2,28 +2,34 @@ package com.example.hellospring;
 
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.bind.annotation.RequestMapping;
+
 //import org.slf4j.Logger;
 //import org.slf4j.LoggerFactory;
+
 
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 import org.apache.logging.log4j.core.LoggerContext;
+
 
 @RestController
 public class HelloController {
 
     @RequestMapping("/")
     public String index() {
+        
+        //Logger logger = LoggerFactory.getLogger(HelloController.class);
+
         LoggerContext context = (LoggerContext) LogManager.getContext(false);
-        // context.setConfigLocation(new URI("/etc/config/log4j2.xml"));
+        //context.setConfigLocation(new URI("/etc/config/log4j2.xml"));
         Logger logger = LogManager.getLogger(HelloController.class);
-        // Logger logger = LoggerFactory.getLogger(HelloController.class);
 
         logger.trace("+++ TRACE Message");
         logger.debug("+++ DEBUG Message");
         logger.info("+++ INFO Message");
         logger.warn("+++ WARN Message");
-        logger.error("+++ ERROR Message");   
+        logger.error("+++ ERROR Message"); 
+          
         return "Greetings from Azure !  Check out the Logs to see the output...";
     }
 
